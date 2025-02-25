2cfb84d0f15b560e33779ad338b464938c1eb845
M-Aaish <aaishk2020@gmail.com>
2025-02-25 22:16:21 +0500

    Update str_main.py

--- shape_gen/str_main.py
+++ shape_gen/str_main.py
@@ -134,10 +134,8 @@ def image_generator_app():
     uploaded_file = st.file_uploader("Upload an Image", type=["jpg", "jpeg", "png"])
     shape_option = st.selectbox("Select Shape", ["Triangle", "Rectangle", "Circle"])
     num_shapes = st.number_input("Enter the number of shapes to encode:", min_value=1, value=10)
-    if shape_option == "Triangle":
-        max_triangle_size = st.number_input("Enter the maximum triangle size:", min_value=1, value=10)
-        min_triangle_size = st.number_input("Enter the minimum triangle size (for filling gaps):", min_value=1, value=5)
-    elif shape_option in ["Rectangle", "Circle"]:
+    # For rectangles and circles, ask for both min and max size.
+    if shape_option in ["Rectangle", "Circle"]:
         min_size = st.number_input("Enter the minimum size of the shape:", min_value=1, value=10)
         max_size = st.number_input("Enter the maximum size of the shape:", min_value=1, value=15)
     else:
@@ -155,12 +153,7 @@ def image_generator_app():
     if st.button("Generate"):
         if uploaded_file is not None:
             shape = shape_option
-            if shape_option == "Triangle":
-                encoded_image, boundaries = encode(img, shape, output_path="",
-                                                   num_shapes=num_shapes,
-                                                   max_size=max_triangle_size,
-                                                   min_size=min_triangle_size)
-            elif shape_option in ["Rectangle", "Circle"]:
+            if shape_option in ["Rectangle", "Circle"]:
                 encoded_image, boundaries = encode(img, shape, output_path="",
                                                    num_shapes=num_shapes, min_size=min_size, max_size=max_size,
                                                    min_radius=min_size, max_radius=max_size)
